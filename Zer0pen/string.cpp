034f8064e44f53eda18961e87935a13bcbe5eca4
박경민 <46581670+parkgmin@users.noreply.github.com>
2020-09-16 13:25:14 +0900

    1일차1일차1일차

---
+++ Zer0pen/string.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main() {
+	string s1 = "ZER0PEN";
+	string s2 = "17기 화이팅!";
+	string s3 = s1 +" "+s2; 
+	cout << s3;
+	return 0;
+}
+
